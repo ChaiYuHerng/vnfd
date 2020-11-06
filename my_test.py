d17d3eaf6723863733b30387a9f25e0d13b5e7f5
chris60303 <chris60303@gmail.com>
2020-11-06 18:40:07 +0800

    new python

--- my_test.py
+++ my_test.py
@@ -55,46 +55,46 @@ if __name__=="__main__":
             cmds=['sudo systemctl unmask mongodb','sudo systemctl restart mongod','exit']
             IP = '172.24.4.110'
         elif vnf == 'test':
-            cmds=['mkdir test','exit\n']
+            cmds=['mkdir test','exit']
             IP = '172.24.4.23'
         elif vnf == 'upf1':
-            cmds=['cd gtp5g','make','sudo make install','cd ../free5gc-stage3/src/upf','mkdir build','cd build','cmake ..','make -j`nproc`','cd config','rm upfcfg.yaml','cd ../../../..','mv upf1.yaml src/upf/build/config/upfcfg.yaml','sudo nohup ./bin/free5gc-upfd\n','exit']
+            cmds=['cd gtp5g','make','sudo make install','cd ../free5gc-stage3/src/upf','mkdir build','cd build','cmake ..','make -j`nproc`','cd config','rm upfcfg.yaml','cd ../../../..','mv upf1.yaml src/upf/build/config/upfcfg.yaml','cd src/upf/build','sudo nohup ./bin/free5gc-upfd & ','exit']
             IP = '172.24.4.111'
         elif vnf == 'upf2':
-            cmds=['cd gtp5g','make','sudo make install','cd ../free5gc-stage3/src/upf','mkdir build','cd build','cmake ..','make -j`nproc`','cd config','rm upfcfg.yaml','cd ../../../..','mv upf2.yaml src/upf/build/config/upfcfg.yaml','sudo nohup ./bin/free5gc-upfd\n','exit']
+            cmds=['cd gtp5g','make','sudo make install','cd ../free5gc-stage3/src/upf','mkdir build','cd build','cmake ..','make -j`nproc`','cd config','rm upfcfg.yaml','cd ../../../..','mv upf2.yaml src/upf/build/config/upfcfg.yaml','cd src/upf/build','sudo nohup ./bin/free5gc-upfd & ','exit']
             IP = '172.24.4.112'
         elif vnf == 'upf3':
-            cmds=['cd gtp5g','make','sudo make install','cd ../free5gc-stage3/src/upf','mkdir build','cd build','cmake ..','make -j`nproc`','cd config','rm upfcfg.yaml','cd ../../../..','mv upf3.yaml src/upf/build/config/upfcfg.yaml','sudo nohup ./bin/free5gc-upfd\n','exit']
+            cmds=['cd gtp5g','make','sudo make install','cd ../free5gc-stage3/src/upf','mkdir build','cd build','cmake ..','make -j`nproc`','cd config','rm upfcfg.yaml','cd ../../../..','mv upf3.yaml src/upf/build/config/upfcfg.yaml','cd src/upf/build','sudo nohup ./bin/free5gc-upfd & ','exit']
             IP = '172.24.4.113'
         elif vnf == 'nrf':
-            cmds=['cd free5gc-stage3','sudo nohup ./bin/nrf & \n','exit\n']
+            cmds=['cd free5gc-stage3','sudo nohup ./bin/nrf & ','exit']
             IP = '172.24.4.101'
         elif vnf == 'amf':
-            cmds=['cd free5gc-stage3','sudo nohup ./bin/amf & \n','exit\n']
+            cmds=['cd free5gc-stage3','sudo nohup ./bin/amf & ','exit']
             IP = '172.24.4.102'
         elif vnf == 'smf1':
-            cmds=['cd free5gc-stage3','sudo nohup ./bin/smf & \n','exit\n']
+            cmds=['cd free5gc-stage3','sudo nohup ./bin/smf & ','exit']
             IP = '172.24.4.103'
         elif vnf == 'smf2':
-            cmds=['cd free5gc-stage3','sudo nohup ./bin/smf & \n','exit\n']
+            cmds=['cd free5gc-stage3','sudo nohup ./bin/smf & ','exit']
             IP = '172.24.4.114'
         elif vnf == 'smf3':
-            cmds=['cd free5gc-stage3','sudo nohup ./bin/smf & \n','exit\n']
+            cmds=['cd free5gc-stage3','sudo nohup ./bin/smf & ','exit']
             IP = '172.24.4.115'
         elif vnf == 'udr':
-            cmds=['cd free5gc-stage3','sudo nohup ./bin/udr & \n','exit\n']
+            cmds=['cd free5gc-stage3','sudo nohup ./bin/udr & ','exit']
             IP = '172.24.4.104'
         elif vnf == 'pcf':
-            cmds=['cd free5gc-stage3','sudo nohup ./bin/pcf & \n','exit\n']
+            cmds=['cd free5gc-stage3','sudo nohup ./bin/pcf & ','exit']
             IP = '172.24.4.105'
         elif vnf == 'udm':
-            cmds=['cd free5gc-stage3','sudo nohup ./bin/udm & \n','exit\n']
+            cmds=['cd free5gc-stage3','sudo nohup ./bin/udm & ','exit']
             IP = '172.24.4.106'
         elif vnf == 'nssf':
-            cmds=['cd free5gc-stage3','sudo nohup ./bin/nssf & \n','exit\n']
+            cmds=['cd free5gc-stage3','sudo nohup ./bin/nssf & ','exit']
             IP = '172.24.4.107'
         elif vnf == 'ausf':
-            cmds=['cd free5gc-stage3','sudo nohup ./bin/ausf & \n','exit\n']
+            cmds=['cd free5gc-stage3','sudo nohup ./bin/ausf & ','exit']
             IP = '172.24.4.108'
         ssh_jump(IP,cmds)
     print("end")
