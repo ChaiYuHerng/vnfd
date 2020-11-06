dccdf7bae18989efc7f9f3e3382974bc238432ae
chris60303 <chris60303@gmail.com>
2020-11-06 14:07:23 +0800

    new python

--- my_test.py
+++ my_test.py
@@ -3,6 +3,6 @@ import sys
 #sys.path.append("..")
 from my_jump import ssh_jump
 if __name__=="__main__":
-    cmds = ['sudo mkdir test','git clone https://github.com/ChaiYuHerng/free5gc-stage3.git']
+    cmds = ['ls\n','sudo mkdir test\n','git clone https://github.com/ChaiYuHerng/free5gc-stage3.git\n','exit\n']
     IP = '172.24.4.85'
     ssh_jump(IP,cmds)
