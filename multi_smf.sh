ff60fcd045068ffb4f51e8162a29b056b798a313
chris60303 <chris60303@gmail.com>
2020-12-04 14:46:46 +0800

    new vnfd

--- multi_smf.sh
+++ multi_smf.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 openstack vnf create --vnfd-name mongo mongo --vim-name chris_vim --description "mongodb"
-#openstack vnf create --vnfd-name upf upf --vim-name chris_vim --description "upf"
 openstack vnf create --vnfd-name upf1 upf1 --vim-name chris_vim --description "upf1"
 sleep 5
 openstack vnf create --vnfd-name upf2 upf2 --vim-name chris_vim --description "upf2"
 sleep 5
 openstack vnf create --vnfd-name upf3 upf3 --vim-name chris_vim --description "upf3"
-sleep 5
-openstack vnf create --vnfd-name upf4 upf4 --vim-name chris_vim --description "upf4"
+
 
 sleep 10
 
@@ -17,6 +15,10 @@ sleep 10
 openstack vnf create --vnfd-name amf amf --vim-name chris_vim --description "amf"
 sleep 10
 openstack vnf create --vnfd-name smf1 smf1 --vim-name chris_vim --description "smf"
+sleep 10
+openstack vnf create --vnfd-name smf2 smf2 --vim-name chris_vim --description "smf2"
+sleep 10
+openstack vnf create --vnfd-name smf3 smf3 --vim-name chris_vim --description "smf3"
 sleep 10
 openstack vnf create --vnfd-name udr udr --vim-name chris_vim --description "udr"
 sleep 10
